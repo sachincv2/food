60bdd5ad2536b04d98e2d08fad5d6b444e3872b1
sachincv2 <sachincv340@gmail.com>
2020-10-05 22:32:36 +0530

    added splitting in model

--- app/controllers/recipie_controller.rb
+++ app/controllers/recipie_controller.rb
@@ -4,5 +4,7 @@ class RecipieController < ApplicationController
     @recipie = Recipie.find(params[:id])
     # [PR]: Since `Step` is a second model to `Recipe`. You can just use `@recipe.steps` to fetch corresponding steps.
     @steps = Step.where(recipie_id: @recipie.id)
+    @recipieskills = @recipie.recipie_ingredients.split(',')
+    @recipieequipments =@recipie.recipie_equipments.split(',')
   end
 end
