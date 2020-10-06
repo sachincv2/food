b9f955a26a6a88201adceeae2edecca4c6d70aed
sachincv2 <sachincv340@gmail.com>
2020-10-06 11:52:46 +0530

    modified with two issues

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'home/index'
   resources :recipie
-  
-  
   resources :category
   get 'home/dashboard'
   resources :upload
