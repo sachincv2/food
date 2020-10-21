5cad088138200fdc59d1be7ff20613d07e9628cc
sachincv2 <sachincv340@gmail.com>
2020-10-21 10:55:54 +0530

    understood .create and removed @comment.save

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,6 +3,5 @@ class CommentsController < ApplicationController
     def create
         @step = Step.find(params[:step_id])
         @comment = @step.comments.create(comment_num: params[:comment_num],comment_text: params[:comment_text],step_id: params[:step_id],user_id: params[:user_id])
-        @comment.save
     end
 end
